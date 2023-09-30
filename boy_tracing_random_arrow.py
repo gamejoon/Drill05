b5555368f94f05741f2a1dc89531baee2ba1a045
gamejoon <gamejoon0325@naver.com>
2023-10-01 03:23:00 +0900

    Update run_motion()

--- boy_tracing_random_arrow.py
+++ boy_tracing_random_arrow.py
@@ -1,16 +1,6 @@
 from pico2d import *
 import random
 
-TUK_WIDTH, TUK_HEIGHT = 1280, 1024
-
-open_canvas(TUK_WIDTH, TUK_HEIGHT)
-
-character = load_image("run_animation.png")
-hand = load_image("hand_arrow.png")
-background = load_image("TUK_GROUND.png")
-
-running = True
-
 def handle_event():
     global running
 
@@ -23,9 +13,40 @@ def handle_event():
             if event.key == SDLK_ESCAPE:
                 running = False
 
+def random_arrow_location():
+    global arrow_location
+    arrow_location = [random.randint(0, TUK_WIDTH - 1), random.randint(0, TUK_HEIGHT - 1)]
+
+def run_motion():
+    global frame, x, y
+    
+    character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    frame = (frame + 1) % 8
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+character = load_image("run_animation.png")
+arrow = load_image("hand_arrow.png")
+background = load_image("TUK_GROUND.png")
+
+running = True
+
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame = 0
+
+arrow_location = []
+
+random_arrow_location()
+
 while running:
     background.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    arrow.draw(arrow_location[0], arrow_location[1])
+    run_motion()
+
     handle_event()
     update_canvas()
+    delay(0.05)
 
 close_canvas()
