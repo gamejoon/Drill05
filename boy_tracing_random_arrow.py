99f71d639eb683b6c14c3d1615da8c85b76dc1e6
gamejoon <gamejoon0325@naver.com>
2023-10-01 02:57:58 +0900

    created boy_tracing_random_arrow.py

---
+++ boy_tracing_random_arrow.py
@@ -0,0 +1,17 @@
+from pico2d import *
+import random
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+character = load_image("run_animation.png")
+hand = load_image("hand_arrow.png")
+background = load_image("TUK_GROUND.png")
+
+running = True
+
+while running:
+    pass
+
+close_canvas()
